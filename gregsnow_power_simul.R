d7b3576edb6319031f867547556dfa80fe694a2c
MTL <mtliuzza@gmail.com>
2015-02-27 18:34:32 +0100

    there was a missing parenthesis

--- gregsnow_power_simul.R
+++ gregsnow_power_simul.R
@@ -24,7 +24,7 @@ sim1 <- function(J, K, b0= 0, bRWA=0, bGroup1=0, bGroup2=0, bGxR1=0,bGxR2=0,Vsub
     
     # put it all together
     pTrust <- 1 / ( 1 + exp(-(b0 + bRWA*RWA+bGroup1*(Group=='I') + #builds probaility of trusting, given the coefficients plus random intercept plus residual error 
-    bGroup2*(Group=='G') +bGxR1*(Group=='I')*RWA+bGxR2*(Group=='G')*RWA+S.re))# + eps)))
+    bGroup2*(Group=='G') +bGxR1*(Group=='I')*RWA+bGxR2*(Group=='G')*RWA+S.re)))# + eps)))
     Trust<-rbinom(J*K,1,pTrust)#build a binomial random distribution for J*K trials based on the probability given the intercepts, the random error and the residual
     # put into a data frame
     mydata <- data.frame( ID = ID, 
