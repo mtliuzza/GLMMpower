28a2643e7bfcb3e1e3fed71b6e0941365360b98b
MTL <mtliuzza@gmail.com>
2015-02-27 17:43:11 +0100

    added rbinom

--- gregsnow_power_simul.R
+++ gregsnow_power_simul.R
@@ -1,27 +1,31 @@
 #http://glmm.wikidot.com/power-analysis
 #https://stat.ethz.ch/pipermail/r-sig-mixed-models/2009q1/001790.html
+
+#this script models one random intercept only (VSubj)
+#in case you want to model random slopes, add them too, e.g. VbRWA=1, VbGrouo=.5 etc...
 library(lme4)
 
 sim1 <- function(J, K, b0= 0, bRWA=0, bGroup1=0, bGroup2=0, bGxR1=0,bGxR2=0,Vsubj=1, Verror=1) {
     Subject <- rep( 1:J)#define subjects
-    ID <-as.factor(Subject)#
+    ID <-as.factor(Subject)#define ID as factor
     
-    RWA<-rnorm(J)
-    RWA<-rep(RWA,1,each=K)
+    RWA<-rnorm(J)#create J (N clusters/participants) normally distributed RWAs
+    RWA<-rep(RWA,1,each=K)#repeat K (observations) times
     
-    Group <- rep(c('T','G','I'), each=1,J*K/3)
+    Group <- rep(c('T','G','I'), each=1,J*K/3)#idem for groups
     
     # random effects per subject
-    S.re <- rnorm(J, b0, sqrt(Vsubj))
-    S.re<-rep(S.re,1,each=K)
+    S.re <- rnorm(J, b0, sqrt(Vsubj))#random effect for the intercept
+    S.re<-rep(S.re,1,each=K)#repeat K (observations) times
     
     # epsilons
-    eps <- rnorm(J*K, 0, sqrt(Verror))
+    eps <- rnorm(J*K, 0, sqrt(Verror))#residual error
     
     
     # put it all together
-    pTrust <- 1 / ( 1 + exp(-(b0 + bRWA*RWA+bGroup1*(Group=='I') + bGroup2*(Group=='G') +bGxR1*(Group=='I')*RWA+bGxR2*(Group=='G')*RWA+S.re + eps)))
-    Trust<-ifelse(pTrust < .5,0,1)
+    pTrust <- 1 / ( 1 + exp(-(b0 + bRWA*RWA+bGroup1*(Group=='I') + #builds probaility of trusting, given the coefficients plus random intercept plus residual error 
+    bGroup2*(Group=='G') +bGxR1*(Group=='I')*RWA+bGxR2*(Group=='G')*RWA+S.re + eps)))
+    Trust<-rbinom(J*K,1,pTrust)#build a binomial random distribution for J*K trials based on the probability given the intercepts, the random error and the residual
     # put into a data frame
     mydata <- data.frame( ID = ID, 
                           RWA = RWA, Group=Group,
@@ -33,15 +37,7 @@ sim1 <- function(J, K, b0= 0, bRWA=0, bGroup1=0, bGroup2=0, bGxR1=0,bGxR2=0,Vsub
     anova(fit2,fit1)[2,8]
 }
 
-#come sono arrivato al .13.
-#http://imaging.mrc-cbu.cam.ac.uk/statswiki/FAQ/effectSize
-#partial eta^2 medium = .13
-#Cohen's f = Square Root of eta-squared / (1-eta-squared)
-#Use the square of a Pearson correlation for effect sizes for partial η2 (R-squared in a multiple regression) 
-#giving 0.01 (small), 0.09 (medium) and 0.25 (large) which are intuitively larger values than eta-squared.
-#Further to this Cohen, Cohen, West and Aiken (2003) on page 95 of Applied Multiple Regression/Correlation Analysis for the behavioral Sciences third edition for looking at semi-partial effects of single predictors in a regression rather than an overall model R-squared 
-#ie looking at sqrt(change in R-squared) from a model with and without the regressor and using the Pearson correlations as a rule of thumb for effect sizes.
-#cohen's f =sqrt(.14^2 / (1-.14^2))=.14 ovvero un efetto piccolo
+#try with .4 as exp(.4)=1.5 odd ratio, that is a small effect,i.e. an increase of 10%
 
 nsims=100
 pb <- txtProgressBar(max=nsims,style=3) # or tkProgressBar or txtProgressbar
